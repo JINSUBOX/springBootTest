3bb79cf7a26270e97cbc31c8bf5da1962c09c525
Jinsup9k <01077717141p@gmail.com>
2020-05-02 20:03:52 +0900

    final test

--- src/test/java/com/jinsu/pak/springBootTest/web/IndexControllerTest.java
+++ src/test/java/com/jinsu/pak/springBootTest/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/",String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("박진수가 만든 게시판");
     }
 }
